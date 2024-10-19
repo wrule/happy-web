fade5ff5fbe0fab7b675a359e6a6668b56e6f706
jimao <shihao.gu@perfma.com>
2024-10-19 22:00:10 +0800

    prefixer

--- index.ts
+++ index.ts
@@ -3,15 +3,17 @@ import type { Request, Response, NextFunction } from 'express';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 import type { Filter, Options, RequestHandler } from 'http-proxy-middleware';
 
-const app = express();
+function prefixer(port: number) {
+  const app = express();
+  const proxyMiddleware = createProxyMiddleware<Request, Response>({
+    target: 'http://www.baidu.com',
+    changeOrigin: true,
+    headers: { Connection: 'keep-alive' },
+    cookieDomainRewrite: '',
+  });
+  app.use('/jimao', proxyMiddleware);
+  app.listen(port);
+  console.log(`prefixer works on port`, port);
+}
 
-const proxyMiddleware = createProxyMiddleware<Request, Response>({
-  target: 'http://www.baidu.com',
-  changeOrigin: true,
-  headers: { Connection: 'keep-alive' },
-  cookieDomainRewrite: '',
-});
-
-app.use('/jimao', proxyMiddleware);
-
-app.listen(3000);
+prefixer(8821);
