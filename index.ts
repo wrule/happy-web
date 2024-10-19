3d5a2e030c8844392928812a5aee5ff6af0ef8b9
jimao <shihao.gu@perfma.com>
2024-10-19 22:11:23 +0800

    ProxyConfig

--- index.ts
+++ index.ts
@@ -1,16 +1,20 @@
 import express from 'express';
 import type { Request, Response, NextFunction } from 'express';
+import { IncomingMessage, ServerResponse } from 'http';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 import type { Filter, Options, RequestHandler } from 'http-proxy-middleware';
 
+type ProxyConfig = Options<express.Request<ParamsDictionary, any, any, QueryString.ParsedQs, Record<string, any>>, express.Response<any, Record<string, any>>>;
+
 function prefixer(port: number) {
   const app = express();
-  const proxyMiddleware = createProxyMiddleware<Request, Response>({
+  const a: ProxyConfig = {
     target: 'http://www.baidu.com',
     changeOrigin: true,
     headers: { Connection: 'keep-alive' },
     cookieDomainRewrite: '',
-  });
+  };
+  const proxyMiddleware = createProxyMiddleware<Request, Response>(a);
   app.use('/jimao', proxyMiddleware);
   app.listen(port);
   console.log(`prefixer works on port`, port);
