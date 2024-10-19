712eede142acd8a3f8c3e49b8fb5784a5e08f381
jimao <shihao.gu@perfma.com>
2024-10-19 23:26:19 +0800

    jsdom

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import type { Request, Response, NextFunction } from 'express';
 import { IncomingMessage, ServerResponse } from 'http';
 import { createProxyMiddleware, responseInterceptor } from 'http-proxy-middleware';
 import type { Filter, Options, RequestHandler } from 'http-proxy-middleware';
+import { JSDOM } from 'jsdom';
 
 type ProxyConfig = Parameters<typeof createProxyMiddleware<Request, Response>>[0];
 export
@@ -17,6 +18,7 @@ function prefixer(port: number, configs: ProxyConfigExt[]) {
       changeOrigin: true,
       headers: { Connection: 'keep-alive' },
       cookieDomainRewrite: '',
+      selfHandleResponse: true,
       ...config,
     });
     console.log('prefixer', `http://127.0.0.1:${port}${config.prefix}`, '->', config.target);
@@ -32,6 +34,18 @@ prefixer(8821, [
     target: 'http://xfiregod.perfma-inc.com',
     on: {
       proxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+        const contentType = proxyRes.headers['content-type']?.toLowerCase() ?? '';
+        if (contentType.startsWith('text/html')) {
+          const html = responseBuffer.toString('utf8');
+          const htmlDOM = new JSDOM(html);
+          htmlDOM.window.document.querySelectorAll('script, img, iframe').forEach((item: any) => {
+            item.src = '/jimao/huoshenshan' + item.src;
+          });
+          htmlDOM.window.document.querySelectorAll('link').forEach((item) => {
+            item.href = '/jimao/huoshenshan' + item.href;
+          });
+          return htmlDOM.serialize();
+        }
         return responseBuffer;
       }),
     },
