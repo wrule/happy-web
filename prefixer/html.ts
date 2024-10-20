78822d2c8acd09970635fbe3466f39e77fbc1f4f
jimao <shihao.gu@perfma.com>
2024-10-20 10:45:08 +0800

    defer

--- prefixer/html.ts
+++ prefixer/html.ts
@@ -3,6 +3,7 @@ import { JSDOM } from 'jsdom';
 export default
 function htmlPrefixer(prefix: string, html: string) {
   const htmlDOM = new JSDOM(html);
+  const document = htmlDOM.window.document;
   htmlDOM.window.document.querySelectorAll('script, img, iframe')
     .forEach((item: any) => {
       item.src = prefix + item.src;
@@ -11,5 +12,16 @@ function htmlPrefixer(prefix: string, html: string) {
     .forEach((item) => {
       item.href = prefix + item.href;
     });
+
+  const prefixerScript = document.createElement('script');
+  if (document.head.firstChild) {
+    document.head.insertBefore(prefixerScript, document.head.firstChild);
+  } else {
+    document.head.appendChild(prefixerScript);
+  }
+  prefixerScript.defer = true;
+  prefixerScript.removeAttribute('src');
+  prefixerScript.textContent = 'console.log(1234);';
+
   return htmlDOM.serialize();
 }
